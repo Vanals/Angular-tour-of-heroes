3855beb653dede2b57a59ebe53b1fddc7a5393dd
Vanals <marco91vanali@gmail.com>
2018-03-13 20:13:23 +0000

    Heroes constant and iterate it in the HTML template of the heroes component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr.Nice' },
+  { id: 12, name: 'Global' },
+  { id: 13, name: 'Kurd' },
+  { id: 14, name: 'Kasty' },
+  { id: 15, name: 'Losty' },
+  { id: 16, name: 'Barrow' }
+];
